b26b9fd7277bb8a0ca5eac3207177bfb7b3b6902
molandim <marcelo.olandim@theculturetrip.com>
2019-04-03 16:47:17 +0100

    things near me

--- googleHome.js
+++ googleHome.js
@@ -18,6 +18,41 @@ app.intent("get location", async (conv, input) => {
   
 })
 
+app.intent('around me', conv => {
+  conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
+  return conv.ask(new Permission({
+      context: 'to locate you',
+      permissions: conv.data.requestedPermission,
+  }));
+})
+ 
+app.intent('user_info', async (conv, params, permissionGranted) => {
+  if (permissionGranted) {
+      const {
+          requestedPermission
+      } = conv.data;
+      if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
+
+           const city = conv.device.location.city;
+
+          if (city) {
+              return await actions.getLocation(city)
+              .then((result) => {
+                return conv.ask(result);
+              })
+          } else {
+              // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates
+              // and a geocoded address on voice-activated speakers.
+              // Coarse location only works on voice-activated speakers.
+              return conv.close('Sorry, I could not figure out where you are.');
+          }
+
+      }
+  } else {
+      return conv.close('Sorry, permission denied.');
+  }
+});
+
 express().use(bodyParser.json(), app).listen(3000, () => {
   console.log(`Express server listening on port 3000`);
 });
