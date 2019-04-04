a24689dbf70abf7dbd746ef42a874a618afce8c9
Hardik Mandavia <hardik.mandavia@theculturetrip.com>
2019-04-04 12:09:08 +0100

    updated controller

--- controller.js
+++ controller.js
@@ -3,32 +3,26 @@ const axios = require("axios");
 class controller {
   constructor() {
     this.getLocation = this.getLocation.bind();
-    this.getLocationResolved = this.getLocationResolved.bind();
-  }
-
-  getLocationResolved(req, res) {
-    const { location } = req.params;
-    const url = `https://app.theculturetrip.com/cultureTrip-api/v1/articles?s=${location}&offset=0&limit=12`
-    axios.default.get(url)
-      .then(result => result.data)
-      .then((data) => {
-        const title = data.articleResources[0].title;
-        res.send(title);
-      });
+    this.getWishlist = this.getWishlist.bind();
   }
 
   getLocation(location) {
-    const url = `https://app.theculturetrip.com/cultureTrip-api/v1/articles?s=${location}&offset=0&limit=12`
+    const url = `https://app.theculturetrip.com/cultureTrip-api/v1/articles?s=${location}&offset=0&limit=10`;
+
     return axios.default.get(url)
       .then(result => result.data)
       .then((data) => {
-        const article = data.articleResources[0];
-        return article;
+        const articles = data.articleResources;
+
+        const length = articles.length;
+        const randomIndex = (Math.floor(Math.random() * Math.floor(length)));
+        return articles[randomIndex];
       });
   }
 
   getWishlist() {
-    const url = 'https://app.theculturetrip.com/cultureTrip-api/v2/users/me/bookmarks/'
+    const url = 'https://app.theculturetrip.com/cultureTrip-api/v2/users/me/bookmarks/';
+
     return axios.default.get(url, { headers: { authorisation: 'bearer TestUserAccessToken'}})
       .then(result => result.data)
       .then(data => {
@@ -39,5 +33,23 @@ class controller {
         return wishlist[randomIndex];
       });
   }
+
+  getLocationWithCategory() {
+    const url = `https://app.theculturetrip.com/cultureTrip-api/v1/articles?s=${location}&offset=0&limit=20`;
+
+    return axios.default.get(url)
+      .then(result => result.data)
+      .then((data) => {
+        const articles = data.articleResources;
+
+        const length = articles.length;
+        const randomIndex = (Math.floor(Math.random() * Math.floor(length)));
+        return articles[randomIndex];
+      });
+  }
+
+
+
+
 }
 exports.controller = controller;
