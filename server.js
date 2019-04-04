5f4b338f5de1f407149d080956578b37e99e739e
molandim <molandim@gmail.com>
2019-04-04 12:10:32 +0100

    my hange

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const controller = require("./controller");
 
 const app = dialogflow();
 
-app.intent("get location", async (conv, input) => {
-  // const location = input["geo-city"];
-  const actions = new controller.controller();
-  await actions.getLocation(input["geo-city"])
-    .then((result) => {
-      const hasScreen = conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT');
+const actions = new controller.controller();
+
+const giveCityResponse = (result, conv) => {
+  const hasScreen = conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT');
 
       if (hasScreen) {
         conv.ask(
@@ -31,12 +29,33 @@ app.intent("get location", async (conv, input) => {
         })
       );
     } else {
-      conv.ask(result.title);
+
+      //onv.ask("I've found " + result.title);
+      conv.contexts.set("getlocation-followup", 5, { description: result.description });
+      conv.ask(`I've found ${result.title}. Would you like to know more?`);
+      //return conv.ask("I've found ", 'is is rfeally nice', 'okay');
+      //console.log(response);
+      
+
+      //conv.data.description = result.description;
+
+    
     }
+}
+
+app.intent("get location - yes", conv => {
+  console.log('i wa here;=');
+  conv.ask(new SimpleResponse({speech: 'something'}));
+});
+
+
+app.intent("get location", async (conv, input) => {
+  return await actions.getLocation(input["geo-city"])
+    .then((result) => {
+      return giveCityResponse(result, conv);
   });
 });
 
-app.intent
 
 app.intent('around me', conv => {
   conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION';
@@ -45,6 +64,8 @@ app.intent('around me', conv => {
       permissions: conv.data.requestedPermission,
   }));
 })
+
+
  
 app.intent('user_info', async (conv, params, permissionGranted) => {
   if (permissionGranted) {
@@ -52,13 +73,11 @@ app.intent('user_info', async (conv, params, permissionGranted) => {
           requestedPermission
       } = conv.data;
       if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
-
            const city = conv.device.location.city;
-
           if (city) {
               return await actions.getLocation(city)
               .then((result) => {
-                return conv.ask(result);
+                return giveCityResponse(result, conv);
               })
           } else {
               // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates
